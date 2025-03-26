12443b4315243ae22ae2723d8a965cab53340697
Ninad Khargonkar <ninad.khargonkar@gmail.com>
2025-03-26 18:39:08 -0500

    consider the combination of gripper surface pts and gripper mesh pts for collision cost

--- model/hand_opt.py
+++ model/hand_opt.py
@@ -581,15 +581,15 @@ class HandObjectGraspOpt:
 
     def compute_energy_align_dist(self):
 
-        hand_mesh_points = self.target_handmodel.get_fullmesh_points()
+        # hand_mesh_points = self.target_handmodel.get_fullmesh_points()
         hand_mesh_points_ = self.target_handmodel.get_fullmesh_points().clone()
 
         hand_surface_points = self.target_handmodel.get_surface_points()
-        # hand_surface_points_ = hand_surface_points.clone()
+        hand_surface_points_ = hand_surface_points.clone()
 
         npts_object = self.object_point_cloud.size()[0]
         npts_hand = hand_surface_points.size()[1]
-        npts_hand_mesh = hand_mesh_points.size()[1]
+        npts_hand_mesh = hand_mesh_points_.size()[1]
 
         ############ Compute Contact Value ##################
         with torch.no_grad():
@@ -651,9 +651,11 @@ class HandObjectGraspOpt:
         batch_object_point_cloud = batch_object_point_cloud.reshape(
             self.num_particles, 1, npts_object, 3
         )
-        hand_mesh_points = hand_mesh_points_.reshape(
-            self.num_particles, 1, npts_hand_mesh, 3
-        )
+
+        combined_hand_pts = torch.cat((hand_surface_points_, hand_mesh_points_), dim=1)
+        npts_hand_mesh = combined_hand_pts.shape[1]
+        hand_mesh_points = combined_hand_pts.reshape(self.num_particles, 1, -1, 3)
+
         hand_mesh_points = hand_mesh_points.repeat(1, npts_object, 1, 1).transpose(1, 2)
 
         batch_object_point_cloud = batch_object_point_cloud.repeat(
@@ -669,7 +671,7 @@ class HandObjectGraspOpt:
             [self.object_normal_cloud[x, :] for x in hand_object_indices], dim=0
         )
         hand_object_signs = (
-            (hand_object_points - hand_mesh_points_) * hand_object_normal
+            (hand_object_points - combined_hand_pts) * hand_object_normal
         ).sum(dim=2)
         hand_object_signs = (hand_object_signs > 0).float()
         energy_penetration = (hand_object_signs * hand_object_dist).mean(dim=1)
